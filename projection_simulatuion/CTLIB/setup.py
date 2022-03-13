c9b38344ecbcec9cf1475019b9da02d1fa3221ef
qingchaozhang3 <76418835+qingchaozhang3@users.noreply.github.com>
2022-03-12 20:40:04 -0500

    Add files via upload

---
+++ projection_simulatuion/CTLIB/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='ctlib',
+    version='0.2.0',
+    author='Wenjun Xia',
+    ext_modules=[
+        CUDAExtension('ctlib', [
+            'src/ctlib.cpp',
+            'src/fan_ed_kernel.cu',
+            'src/fan_ed_view.cu',
+            'src/fan_ea_kernel.cu',
+            'src/para_kernel.cu',
+            'src/laplacian_cuda_kernel.cu',
+        ]),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
